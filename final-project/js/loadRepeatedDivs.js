1fdfdd0b720367b4356251ad7196d79ea44eed33
pronnoynandy <pronnoynandy@gmail.com>
2016-07-11 14:43:52 -0400

    Add files via upload

---
+++ final-project/js/loadRepeatedDivs.js
@@ -0,0 +1,6 @@
+/*loading repeated divs*/
+
+function loadRepeatedDivs(){
+	$("#navbar").load("loadNavbar.html");
+}
+jQuery(document).ready(function(){loadRepeatedDivs();});
--- final-project/js/myScript.js
+++ final-project/js/myScript.js
@@ -7,7 +7,7 @@
   // Toggle tranparent navbar when the user scrolls the page
 
   $(window).scroll(function() {
-    if($(this).scrollTop() > 650)  /*height in pixels when the navbar becomes non opaque*/ 
+    if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/ 
     {
         $('.opaque-navbar').addClass('opaque');
     } else {
